6c26acba6fb5cc73670af502f25d326ef81165d1
KOALAS2648 <107459548+KOALAS2648@users.noreply.github.com>
2023-03-07 19:19:57 +0000

    added a "temanal"
    Added a "terminal" to the code you don't have to hard code the program's file location.

--- Interprater.py
+++ Interprater.py
@@ -1,12 +1,18 @@
-command_words = ["register","add","out","inc",";","rprint","add","mul"]
+import os
+import time
+import webbrowser
+command_words = ["register","add","out","inc",";","rprint","add","mul","exit"]
 more_info_command_words = ["register"]
 # creates the memeroy lists
-memory = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+memory = []
+for _ in range(0, 100):
+    memory.append(0)
 # checks for any keywords
-def check_word(word, address, value, addaddress):
-    o = 0
+def check_word(word, address, value):
+    o = 0   
     if address < 100:
         try:
+            # puts what ever the value is into the address
             if word == "register" and memory[address] == 0:
                 memory[address] = int(value)
             elif word == "register" and memory[address] != 0:
@@ -16,12 +22,15 @@ def check_word(word, address, value, addaddress):
             elif word == "rprint":
                 return memory
             elif word == "add":
-                memory[address] = memory[adress]+value
+                memory[address] = memory[address]+value
             elif word == "mul":
-                memory[address] = memory[adress]*value
+                memory[address] = memory[address]*value
             elif word == "out":
                 o = memory[address]
-                print(o)
+                if o != None:
+                    print(o)
+            elif word =="exit":
+                exit
         except:
             return "why"
 def my_lang(code):
@@ -53,7 +62,7 @@ def my_lang(code):
         for tests in code_lines:
             counter_inner = 0
             for words in tests:
-                if len(words)<3 and words not in command_words and words != "":
+                if words not in command_words and words != "":
                     code_lines[counter_outer][counter_inner]=int(words)
                     counter_inner+=1
                 elif words in command_words:
@@ -63,12 +72,17 @@ def my_lang(code):
     do_words = []
     address = []
     values = []
-    start_word = ""
+    counter_out = 0
+    #start_word = ""
     counter_outer = 0
     counter = 0
     for coword in code_lines:
-        do_words.append(code_lines[counter_outer][0])
+        p = code_lines[counter_outer][0]
+        do_words.append(p)
+        if p == "out":
+            counter_out +=1
         counter_outer+=1
+        
     counter_outer = 0
     # get the adresses of where to register
     if code_lines[counter_outer][0] in more_info_command_words:
@@ -83,9 +97,23 @@ def my_lang(code):
     #RUNNING THE FINAL PRODUCT
     counter=0
     for command_word in do_words:
-        output =check_word(command_word, address[counter],values[counter],0)
+        output =check_word(command_word, address[counter],values[counter])
         counter+=1
     if __name__=="__main__":
         return output
-
-print(my_lang("program.txt"))
+    
+has_exited = False
+while not has_exited:
+    ask_action = input(">>> ")
+    action = ask_action.split(" ")
+    #print(action)
+    if action[0] == "mylangrun":
+        try:
+            my_lang(action[1])
+        except OSError as e:
+            with open("errors.txt","w")as errors_file:
+                errors_file.write(str(e))
+    
+    elif action[0] == "exit":
+        has_exited = True
+#Desktop/python/my_interprater
